ba9d6fc1dc70bd1e791707a8c8334b63c8038de9
Rayven <73194566+RayvenWilk@users.noreply.github.com>
2021-09-15 13:36:09 -0600

    Update Decastave Spell.js

--- Decastave Spell.js
+++ Decastave Spell.js
@@ -31,5 +31,10 @@ SpellsList["decastave"] = {
 	duration : "1 round",
 	firstCol : "checkbox",
 	description : "You create a quarterstaff of force, 1d6 force dmg. On crit add 1d8 thunder dmg plus con save (DC 14), fail - perm deaf.",
-	descriptionFull : "You conjure into existance a quaterstaff of force, approx. 8ft long & 2in diam. The decastave lasts only so long as you keep it in hand, if you give it to another charater or drop it, it vanishes. (You can cast other spells with somatic components by holding the staff in one hand.) You can use the decastave in combat to strike with a melee attack, dealing 1d6 force damage per hit. Since the decastave is made of force, it affects incorporeal and ethereal creatures normally (provided you can see such creatures). The deastave possesses the thundering weapon ability and deals 1d8 thunder damage on a crit hit. Subjects dealt a crit hit by the decastave much also succeed a constitution save (DC 14) or be defened permanently. Material component: A slender wand of duskwood",
+	descriptionFull : desc([
+		"You conjure into existance a quaterstaff of force, approx. 8ft long & 2in diam. The decastave lasts only so long as you keep it in hand, if you give it to another charater or drop it, it vanishes. (You can cast other spells with somatic components by holding the staff in one hand.)"
+		"You can use the decastave in combat to strike with a melee attack, dealing 1d6 force damage per hit. Since the decastave is made of force, it affects incorporeal and ethereal creatures normally (provided you can see such creatures)."
+		"The deastave possesses the thundering weapon ability and deals 1d8 thunder damage on a crit hit. Subjects dealt a crit hit by the decastave much also succeed a constitution save (DC 14) or be defened permanently."
+		"Material component: A slender wand of duskwood",
+		])
 };
