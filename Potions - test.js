63dc855c239c6d1d9ee5d7d84c34f55b2d757dc0
Rayven <73194566+RayvenWilk@users.noreply.github.com>
2021-09-23 09:06:03 -0600

    Add files via upload

---
+++ Potions - test.js
@@ -0,0 +1,20 @@
+/*	-INFORMATION-
+	Subject:	Potions & Elixirs
+	Effect:		Add common potions to the equipment list
+	Remarks:	This is to add the potions and elixirs created by RPGPapercrafts & Raafling (https://www.drivethrurpg.com/product/241053/Magic-Potions--Ingredients)
+	Coded by :	rayvenwilk
+	Date :		09.22.2021
+	Sheet:		v13.0.5
+*/
+
+var iFileName = "MagicalPotions by RPGPapercrafts(transcribed by rayvenwilk.js";
+
+RequiredSheetVersion("13.0.5");
+
+GearList["blimberry juice"] = {
+        name : "Blimberry Juice",
+        infoname : "Potion, Blimberry Juice",
+        amount : 1,
+        weight : 0.25,
+        source : ["HB"],
+}
