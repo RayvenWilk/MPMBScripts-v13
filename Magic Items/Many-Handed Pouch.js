53dd9cdc70fd4e890186ae55c0bb4eb2a2e6f99d
Rayven <73194566+RayvenWilk@users.noreply.github.com>
2021-10-08 09:41:14 -0600

    Update Many-Handed Pouch.js

--- Magic Items/Many-Handed Pouch.js
+++ Magic Items/Many-Handed Pouch.js
@@ -19,14 +19,23 @@ SourceList["UA:A2"] = {
     url : "https://media.wizards.com/2019/dnd/downloads/UA-Artificer2-2019.pdf",
 };
 
+
+AddFeatureChoice(ClassList.artificer.features["infuse item"], true, "Many-Handed Pouch (prereq: level 4 artificer)", {
+    name : "Many-Handed Pouch",
+    source : ["UA:A2", 13],
+    description : "",
+    magicitemsAdd : ["Many-Handed Pouch"],
+    prereqeval : function(v) { return classes.known.artificer.level >= 4; },
+});
+
 MagicItemsList["many-handed pouch"] = {
     name : "Many-Handed Pouch",
     source : ["UA:A2", 13],
     type : "wondrous item",
-    description : "The infused pouches all share one interdimensional space of the same capacity. Thus, reaching into any of the pouches allows access to the same storage space as long as it is within 100 miles of another one of the pouches; the pouch is otherwise empty and won’t accept any contents.",
-    descriptionFull : "The infused pouches all share one interdimensional space of the same capacity as a single pouch. Thus, reaching into any of the pouches allows access to the same storage space. A pouch operates as long as it is within 100 miles of another one of the pouches; the pouch is otherwise empty and won’t accept any contents. \nIf this infusion ends, the items stored in the shared space move into one of the pouches, determined at random. The rest of the pouches become empty.",
+    description : "The infused pouches (2-5) all share one interdimensional space of the same capacity. Thus, reaching into any of the pouches allows access to the same storage space as long as it is within 100 miles of another one of the pouches; the pouch is otherwise empty and won’t accept any contents.",
+    descriptionFull : "The infused pouches all share one interdimensional space of the same capacity as a single pouch, 2-5 pouches in total. Thus, reaching into any of the pouches allows access to the same storage space. A pouch operates as long as it is within 100 miles of another one of the pouches; the pouch is otherwise empty and won’t accept any contents. \nIf this infusion ends, the items stored in the shared space move into one of the pouches, determined at random. The rest of the pouches become empty.",
     weight : 1,
-    rarity : "common",
-    prerequisite : "Requires Artificer class",
-    prereqeval : function (v) { return classes.known.artificer ? true : false; },
+    rarity : "uncommon",
+    prerequisite : "Requires Artificer class level 4",
+    prereqeval : function(e){return 4<=classes.known.artificer.level},
 }
