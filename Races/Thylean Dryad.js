b205201976efe608390ba6198c71713ffc233bb8
Rayven <73194566+RayvenWilk@users.noreply.github.com>
2022-01-04 12:08:09 -0700

    Add files via upload

---
+++ Races/Thylean Dryad.js
@@ -0,0 +1,92 @@
+/*	-WHAT IS THIS?-
+	This file adds optional material to "MPMB's Character Record Sheet" found at https://flapkan.com/mpmb/charsheets
+	Import this file using the "Add Extra Materials" bookmark.
+	-KEEP IN MIND-
+	It is recommended to enter the code in a fresh sheet before adding any other information (i.e. before making your character with it).
+*/
+
+/*  -INFORMATION-
+	Subject:    
+	Effect:     
+	Content:	
+	Code by:	rayvenwilk
+	Date:		2021-11-18 (sheet 13.0.8)
+*/
+
+var iFileName = "Thylean Dryad.js";
+
+SourceList["OotD"] = {
+	name : "Odyssey of the Dragonlords",
+	abbreviation : "OotD",
+	date : "2020/04/27",
+	group : "Third Party Compendium",
+	url : "https://www.kickstarter.com/projects/arcanumworlds/odyssey-of-the-dragonlords-5th-edition-adventure-b",
+	defaultExcluded : true
+};
+RaceList["thylean dryad"] = {
+	regExpSearch : /^(?=.*thylean)((?=.*(forest|tree).*nymph)|(?=.*dryad)).*$/i,
+	name : "Thylean Dryad",
+	sortname : "Dryad, Thylean",
+	source : ["OotD", 317],
+	plural : "Thylean Dryads",
+	size : 3,
+	speed : {
+		walk : { spd : 30, enc : 20 }
+	},
+	skills : ["Persuasion"],
+	languageProfs : ["Common", "Sylvan"],
+	vision : ["Adv. on Wisdom (Survival) checks in forested areas", 0],
+	age : " are born in cocoons where they remain for around 100 years before emerging. They can live for as long as 1000 years.",
+	height : " range from barely 5 to well over 6 feet tall",
+	weight : " weigh around 165 lbs",
+	heightMetric : " range from barely 1,5 to well over 1,8 metres tall",
+	weightMetric : " weigh around 75 kg",
+	scorestxt : "+2 Charisma and +1 Wisdom",
+	scores : [0, 0, 0, 0, 1, 2],
+	trait : "Thylean Dryad (+2 Charisma +1 Wisdom)" + desc([
+		"Enthralling Beauty: I am proficient in the Persuasion skill. I can cast the Charm Person spell once per short rest as a 1st-level spell.",
+		"Dryad Ancestry: I can speak to beasts and plants. I have advantage on Wisdom (Survival) checks in forested areas. Starting at level 3 I can cast Goodberry. Starting at level 7 I can cast Barkskin. Both spells can be used once per short rest. Charisma is my spellcasting ability for these."
+	]),
+	spellcastingAbility : 6,
+	features : {
+		"charm person" : {
+			name : "Enthralling Beauty",
+			limfeaname : "Charm Person",
+			minlevel : 1,
+			usages : 1,
+			recovery : "short rest",
+			spellcastingBonus : {
+				name : "Enthralling Beauty",
+				spells : ["charm person"],
+				selection : ["charm person"],
+				firstCol : 'oncesr'
+			}
+		},
+		"goodberry" : {
+			name : "Dryad Ancestry (level 3)",
+			limfeaname : "Goodberry",
+			minlevel : 3,
+			usages : 1,
+			recovery : "short rest",
+			spellcastingBonus : {
+				name : "Dryad Ancestry (level 3)",
+				spells : ["goodberry"],
+				selection : ["goodberry"],
+				firstCol : 'oncesr'
+			}
+		},
+		"barkskin" : {
+			name : "Dryad Ancestry (level 7)",
+			limfeaname : "Barkskin",
+			minlevel : 7,
+			usages : 1,
+			recovery : "short rest",
+			spellcastingBonus : {
+				name : "Dryad Ancestry (level 7)",
+				spells : ["barkskin"],
+				selection : ["barkskin"],
+				firstCol : 'oncesr'
+			}
+		}
+	}
+}
